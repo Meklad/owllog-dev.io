8d3a2172edcb10e3691d970f105c23dc127ba973
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2020-08-02 23:25:06 +0200

    #comment Remove outline from chat input #type MOD #time 5m

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @stack('styles')
 </head>
 <body>
     <div id="app">
@@ -79,5 +81,6 @@
             @yield('content')
         </main>
     </div>
+    @stack('scripts')
 </body>
 </html>
