1a30978e57a498ccb91b10e5ae1de42199a6be33
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2020-08-03 00:22:23 +0200

    #comment Modidy Models for Message and User by adding some columns #type MOD #time 5m

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar'
     ];
 
     /**
@@ -40,5 +40,10 @@ class User extends Authenticatable
     public function getNameAttribute($value)
     {
         return ucfirst($value);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(\App\Message::class, 'user_id');
     }
 }
